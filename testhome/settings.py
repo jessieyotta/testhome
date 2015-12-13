cc6158518928fcaa213d0b64a44447a6aaa13d7a
jessieyotta <jessie@yottagreen.com>
2015-12-13 08:46:41 +0000

    initial commit

--- testhome/settings.py
+++ testhome/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'testhome.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'yottadb',
+	'USER': 'yottagreen',
+	'PASSWORD': 'yottagreen1',
+	'HOST': 'yottadbinstance.cfiwsjqhd2mo.eu-west-1.rds.amazonaws.com',
+	'PORT': '3306',
+
     }
 }
 
