e3fcdd6347a7d770741c6472f4be2fbeae7df62e
jessieyotta <jessie@yottagreen.com>
2015-12-13 09:44:30 +0000

    updated settings, created app, added south, enabled django admin

--- testhome/settings.py
+++ testhome/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'testhomeapp',
+    'south',   
 ]
 
 MIDDLEWARE_CLASSES = [
